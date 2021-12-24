dafdc8b94a5b3345c5784d91311aab1b67bd22c7
Graham Preston <graham.preston@gmail.com>
2021-12-24 07:59:14 -0500

    Basic Lobby table

--- lib/cuberacer_live_web/live/game_live/lobby.ex
+++ lib/cuberacer_live_web/live/game_live/lobby.ex
@@ -4,38 +4,6 @@ defmodule CuberacerLiveWeb.GameLive.Lobby do
   alias CuberacerLive.Sessions
 
   @impl true
-  def render(assigns) do
-    ~H"""
-    <h1>Lobby</h1>
-
-    <div>
-    <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-
-        <th></th>
-      </tr>
-    </thead>
-    <tbody id="sessions">
-      <%= for session <- @active_sessions do %>
-        <tr id={"session-#{session.id}"}>
-          <td><%= session.name %></td>
-
-          <td>
-            <span>
-              <%= live_redirect "Join", to: Routes.game_room_path(@socket, :show, session.id) %>
-            </span>
-          </td>
-        </tr>
-      <% end %>
-    </tbody>
-    </table>
-    </div>
-    """
-  end
-
-  @impl true
   def mount(_params, _session, socket) do
     if connected?(socket), do: Sessions.subscribe()
     {:ok, fetch(socket)}
