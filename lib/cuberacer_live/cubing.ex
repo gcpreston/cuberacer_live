b51e44103c9d308e07c293c7ad0d6788ec333f4b
Graham Preston <graham.preston@gmail.com>
2022-01-07 14:54:49 -0500

    list_cube_types/0 + docs and tests

--- lib/cuberacer_live/cubing.ex
+++ lib/cuberacer_live/cubing.ex
@@ -5,8 +5,33 @@ defmodule CuberacerLive.Cubing do
   import Ecto.Query, only: [from: 2]
 
   alias CuberacerLive.Repo
-  alias CuberacerLive.Cubing.Penalty
+  alias CuberacerLive.Cubing.{CubeType, Penalty}
 
+  @doc """
+  Returns the list of cube types.
+
+  ## Examples
+
+      iex> list_cube_types()
+      [%CubeType{}, ...]
+
+  """
+  def list_cube_types do
+    Repo.all(CubeType)
+  end
+
+  @doc """
+  Get a penalty by name.
+
+  ## Examples
+
+      iex> get_penalty("+2")
+      %Penalty{name: "+2"}
+
+      iex> get_penalty("bad val")
+      nil
+
+  """
   def get_penalty(name) do
     query = from p in Penalty, where: p.name == ^name
     Repo.one(query)
