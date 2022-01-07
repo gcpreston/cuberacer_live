a7666f2f3baa0de941a5a873e0deb79fc44f5e16
Graham Preston <graham.preston@gmail.com>
2022-01-07 14:55:44 -0500

    Lobby :new

--- lib/cuberacer_live_web/live/game_live/lobby.ex
+++ lib/cuberacer_live_web/live/game_live/lobby.ex
@@ -2,11 +2,29 @@ defmodule CuberacerLiveWeb.GameLive.Lobby do
   use CuberacerLiveWeb, :live_view
 
   alias CuberacerLive.Sessions
+  alias CuberacerLive.Sessions.Session
 
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket), do: Sessions.subscribe()
     {:ok, fetch(socket)}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Lobby")
+    |> assign(:session, nil)
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Room")
+    |> assign(:session, %Session{})
   end
 
   defp fetch(socket) do
--- test/cuberacer_live_web/live/game_live/lobby_test.exs
+++ test/cuberacer_live_web/live/game_live/lobby_test.exs
@@ -3,19 +3,73 @@ defmodule CuberacerLive.GameLive.LobbyTest do
 
   import Phoenix.LiveViewTest
   import CuberacerLive.SessionsFixtures
+  import CuberacerLive.CubingFixtures
 
-  defp create_session(_) do
-    session = session_fixture() |> CuberacerLive.Repo.preload(:cube_type)
-    %{session: session}
+  describe ":index" do
+    test "displays all sessions", %{conn: conn} do
+      session = session_fixture() |> CuberacerLive.Repo.preload(:cube_type)
+      {:ok, _live, html} = live(conn, Routes.game_lobby_path(conn, :index))
+
+      assert html =~ "Lobby"
+      assert html =~ session.name
+      assert html =~ session.cube_type.name
+    end
+
+    test "patches to new room modal", %{conn: conn} do
+      {:ok, live, _html} = live(conn, Routes.game_lobby_path(conn, :index))
+
+      live
+      |> element("#t_new-room")
+      |> render_click()
+
+      assert_patch(live, Routes.game_lobby_path(conn, :new))
+    end
   end
 
-  setup [:create_session]
+  describe ":new" do
+    test "displays create room modal correctly", %{conn: conn} do
+      cube_type_1 = cube_type_fixture(name: "2x2")
+      cube_type_2 = cube_type_fixture(name: "3x3")
+      {:ok, _live, html} = live(conn, Routes.game_lobby_path(conn, :new))
 
-  test "displays all sessions", %{conn: conn, session: session} do
-    {:ok, _index_live, html} = live(conn, Routes.game_lobby_path(conn, :index))
+      html
+      |> assert_html("h2", text: "New Room")
+      |> assert_html(".t_room-name-input", count: 1)
+      |> assert_html(".t_room-cube-type-input", count: 1)
+      |> assert_html("option[value=#{cube_type_1.id}", count: 1)
+      |> assert_html("option[value=#{cube_type_2.id}", count: 1)
+      |> assert_html(".t_room-save", count: 1)
+    end
 
-    assert html =~ "Lobby"
-    assert html =~ session.name
-    assert html =~ session.cube_type.name
+    test "create room modal creates a new room", %{conn: conn} do
+      cube_type = cube_type_fixture()
+      {:ok, live, html} = live(conn, Routes.game_lobby_path(conn, :new))
+
+      refute_html(html, ".t_room")
+
+      result =
+        live
+        |> form("#create-room-form")
+        |> render_submit(%{session: %{name: "new session", cube_type_id: cube_type.id}})
+
+      assert_redirect(live, Routes.game_lobby_path(conn, :index))
+
+      {:ok, _live, html} = follow_redirect(result, conn)
+
+      assert_html(html, ".t_room", count: 1)
+    end
+
+    test "displays error messages", %{conn: conn} do
+      cube_type = cube_type_fixture()
+      {:ok, live, _html} = live(conn, Routes.game_lobby_path(conn, :new))
+
+      html =
+        live
+        |> form("#create-room-form")
+        |> render_submit(%{session: %{name: "", cube_type_id: cube_type.id}})
+
+      refute_redirected(live, Routes.game_lobby_path(conn, :index))
+      assert_html(html, ~s(.invalid-feedback[phx-feedback-for="session[name]"), text: "can't be blank")
+    end
   end
 end
