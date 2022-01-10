2d1081bbdf57c0d14c24f74dc9eacbc0e2d723a2
Graham Preston <graham.preston@gmail.com>
2022-01-10 14:46:31 -0500

    Basic login and register pages

--- test/cuberacer_live_web/controllers/user_session_controller_test.exs
+++ test/cuberacer_live_web/controllers/user_session_controller_test.exs
@@ -11,9 +11,9 @@ defmodule CuberacerLiveWeb.UserSessionControllerTest do
     test "renders log in page", %{conn: conn} do
       conn = get(conn, Routes.user_session_path(conn, :new))
       response = html_response(conn, 200)
-      assert response =~ "<h1>Log in</h1>"
+      assert response =~ "Log in to Cuberacer</h1>"
       assert response =~ "Register</a>"
-      assert response =~ "Forgot your password?</a>"
+      assert response =~ "Forgot password?</a>"
     end
 
     test "redirects if already logged in", %{conn: conn, user: user} do
@@ -75,7 +75,7 @@ defmodule CuberacerLiveWeb.UserSessionControllerTest do
         })
 
       response = html_response(conn, 200)
-      assert response =~ "<h1>Log in</h1>"
+      assert response =~ "Log in to Cuberacer</h1>"
       assert response =~ "Invalid email or password"
     end
   end
