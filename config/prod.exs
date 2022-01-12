aebbe85d52607c9e6a55f7797d14055133925dc6
Graham Preston <graham.preston@gmail.com>
2022-01-12 14:33:47 -0500

    Try live.cuberacer.io

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :cuberacer_live, CuberacerLiveWeb.Endpoint,
   load_from_system_env: true,
   http: [port: {:system, "PORT"}],
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "cuberacer.io", port: 443],
+  url: [host: "live.cuberacer.io", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
