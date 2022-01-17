9db44d093551f6c5e20938df54ed7e745f062a6f
Graham Preston <graham.preston@gmail.com>
2022-01-16 20:52:27 -0500

    Bump Whisk

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule CuberacerLive.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:whisk, "~> 0.0.1"},
+      {:whisk, "~> 0.0.2"},
       {:distillery, "~> 2.1"}
     ]
   end
