e08c5edca18b8e5ec451e7762c6f9b2baef158f6
Graham Preston <graham.preston@gmail.com>
2022-01-19 10:33:07 -0500

    Optimization

--- lib/cuberacer_live_web/controllers/user_session_controller.ex
+++ lib/cuberacer_live_web/controllers/user_session_controller.ex
@@ -11,14 +11,17 @@ defmodule CuberacerLiveWeb.UserSessionController do
   def create(conn, %{"user" => user_params}) do
     %{"username_or_email" => username_or_email, "password" => password} = user_params
 
-    cond do
-      user = Accounts.get_user_by_username_and_password(username_or_email, password) ->
-        UserAuth.log_in_user(conn, user, user_params)
-      user = Accounts.get_user_by_email_and_password(username_or_email, password) ->
-        UserAuth.log_in_user(conn, user, user_params)
-      true ->
-        # In order to prevent user enumeration attacks, don't disclose whether the username/email is registered.
-        render(conn, "new.html", error_message: "Invalid credentials")
+    user =
+      if String.contains?(username_or_email, "@") do
+        Accounts.get_user_by_email_and_password(username_or_email, password)
+      else
+        Accounts.get_user_by_username_and_password(username_or_email, password)
+      end
+
+    if user do
+      UserAuth.log_in_user(conn, user, user_params)
+    else
+      render(conn, "new.html", error_message: "Invalid credentials")
     end
   end
 
