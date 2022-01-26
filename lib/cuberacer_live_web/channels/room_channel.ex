fdb7388f190829811e74a2c60b6b5f438976bc1c
Graham Preston <graham.preston@gmail.com>
2022-01-25 21:51:24 -0500

    Room channel presence

--- lib/cuberacer_live_web/channels/room_channel.ex
+++ lib/cuberacer_live_web/channels/room_channel.ex
@@ -3,11 +3,23 @@ defmodule CuberacerLiveWeb.RoomChannel do
 
   alias CuberacerLive.Repo
   alias CuberacerLive.Sessions
+  alias CuberacerLiveWeb.Presence
 
   @impl true
   def join("room:" <> room_id, payload, socket) do
+    send(self(), :after_join)
+
     session = Sessions.get_room_data!(room_id)
     {:ok, session, socket}
   end
 
+  @impl true
+  def handle_info(:after_join, socket) do
+    {:ok, _} = Presence.track(socket, socket.assigns.user.id, %{
+      online_at: inspect(System.system_time(:second))
+    })
+
+    push(socket, "presence_state", Presence.list(socket))
+    {:noreply, socket}
+  end
 end
