4240fc7a493e7a4a0509607e7ee1eb5edf55c282
Graham Preston <graham.preston@gmail.com>
2022-01-26 00:51:11 -0500

    Round, solve, penalty, message backend

--- lib/cuberacer_live_web/channels/room_channel.ex
+++ lib/cuberacer_live_web/channels/room_channel.ex
@@ -2,15 +2,42 @@ defmodule CuberacerLiveWeb.RoomChannel do
   use CuberacerLiveWeb, :channel
 
   alias CuberacerLive.Repo
-  alias CuberacerLive.Sessions
+  alias CuberacerLive.{Cubing, Sessions, Messaging}
   alias CuberacerLiveWeb.Presence
 
   @impl true
-  def join("room:" <> room_id, payload, socket) do
+  def join("room:" <> room_id, _payload, socket) do
     send(self(), :after_join)
 
+    Sessions.subscribe(room_id)
+    Messaging.subscribe(room_id)
     session = Sessions.get_room_data!(room_id)
     {:ok, session, socket}
+  end
+
+  @impl true
+  def handle_in("new_round", _payload, socket) do
+    Sessions.create_round(session_id(socket))
+    {:noreply, socket}
+  end
+
+  def handle_in("new_solve", %{"time" => time}, socket) do
+    penalty = Cubing.get_penalty("OK")
+    Sessions.create_solve(session_id(socket), socket.assigns.user.id, time, penalty.id)
+    {:noreply, socket}
+  end
+
+  def handle_in("change_penalty", %{"penalty" => penalty_name}, socket) do
+    if solve = Sessions.get_current_solve(session_id(socket), socket.assigns.user.id) do
+      Sessions.change_penalty(solve, Cubing.get_penalty(penalty_name))
+    end
+
+    {:noreply, socket}
+  end
+
+  def handle_in("send_message", %{"message" => message}, socket) do
+    Messaging.create_room_message(session_id(socket), socket.assigns.user.id, message)
+    {:noreply, socket}
   end
 
   @impl true
@@ -22,4 +49,30 @@ defmodule CuberacerLiveWeb.RoomChannel do
     push(socket, "presence_state", Presence.list(socket))
     {:noreply, socket}
   end
+
+  def handle_info({Sessions, [:round, :created], round}, socket) do
+    round = Repo.preload(round, :solves)
+    broadcast!(socket, "round_created", round)
+    {:noreply, socket}
+  end
+
+  def handle_info({Sessions, [:solve, :created], solve}, socket) do
+    solve = Repo.preload(solve, :penalty)
+    broadcast!(socket, "solve_created", solve)
+    {:noreply, socket}
+  end
+
+  def handle_info({Sessions, [:solve, :updated], solve}, socket) do
+    solve = Repo.preload(solve, :penalty)
+    broadcast!(socket, "solve_updated", solve)
+    {:noreply, socket}
+  end
+
+  def handle_info({Messaging, [:room_message, _], room_message}, socket) do
+    room_message = Repo.preload(room_message, :user)
+    broadcast!(socket, "message_created", room_message)
+    {:noreply, socket}
+  end
+
+  defp session_id(%Phoenix.Socket{topic: "room:" <> id}), do: id
 end
