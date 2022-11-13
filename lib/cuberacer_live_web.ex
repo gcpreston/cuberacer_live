3dc29cb477a44c268aff991064bd546d03016eda
Graham Preston <7387903+gcpreston@users.noreply.github.com>
2022-11-13 12:50:31 -0800

    Fix vendor static path

--- lib/cuberacer_live_web.ex
+++ lib/cuberacer_live_web.ex
@@ -17,7 +17,7 @@ defmodule CuberacerLiveWeb do
   and import those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images vendor favicon.ico robots.txt)
 
   def controller do
     quote do
